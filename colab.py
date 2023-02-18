d4de4f956172969a7c1066e5a76fd49f6efb797c
Yan <yanmarques@gmail.com>
2023-02-18 19:01:06 -0300

    colab: figsize

--- colab.py
+++ colab.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 
 
 class ColabContext:
-    def __init__(self, dpi=100):
+    def __init__(self, figsize=(20, 10), dpi=100):
         self.dpi = dpi
+        self.figsize = figsize
 
     def __enter__(self):
-        self.plt_context = plt.rc_context({"figure.dpi": self.dpi})
+        self.plt_context = plt.rc_context(
+            {"figure.figsize": self.figsize, "figure.dpi": self.dpi}
+        )
         self.plt_context.__enter__()
 
     def __exit__(self, type, value, traceback):
-        pass
+        return
