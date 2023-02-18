d2f1086dcede52358a70df72fae90ddba6c90ee4
Yan <yanmarques@gmail.com>
2023-02-18 19:20:22 -0300

    refactor colab context

--- colab.py
+++ colab.py
@@ -1,16 +1,5 @@
 import matplotlib.pyplot as plt
 
 
-class ColabContext:
-    def __init__(self, figsize=(20, 10), dpi=100):
-        self.dpi = dpi
-        self.figsize = figsize
-
-    def __enter__(self):
-        self.plt_context = plt.rc_context(
-            {"figure.figsize": self.figsize, "figure.dpi": self.dpi}
-        )
-        self.plt_context.__enter__()
-
-    def __exit__(self, type, value, traceback):
-        return
+def colab_context(figsize=(20, 10), dpi=100):
+    return plt.rc_context({"figure.figsize": figsize, "figure.dpi": dpi})
